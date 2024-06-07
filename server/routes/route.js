efbc5afa6a061c9f266b5b58f3e2ea81b72f5206
ABHAY CHAVAN <75495921+abhay113@users.noreply.github.com>
2024-06-07 12:22:35 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../user/userController");
-const userService = require("../user/userService");
 
 router.get("/getall", userController.getAllUsers);
 router.get("/get/:id", userController.getUserById);
